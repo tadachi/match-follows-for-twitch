fcba6e23a4aa68d83a06527cd12c9e05530b1819
Takumi Adachi <takumiadachi@gmail.com>
2015-04-25 19:08:54 -0700

    Implemented prototype for matching follows.

--- www/js/main2.js
+++ www/js/main2.js
@@ -20,18 +20,17 @@ if (!String.prototype.format) {
  * opts = {"offset":25}
  */
 function getAllFollowedStreams(name, opts, callback) {
-    // Verify opts (options)
+    // Verify opts (options).
     var direction = "DESC";
     var limit = 1000;
     var offset = 0 // get the next 25 people if you follow more than 25 people so increase offset by 25.
 
-    if (opts["direction"]) { var direction = opts["direction"]; }
-    if (opts["limit"]) { var limit = opts["limit"]; }
-    if (opts["offset"]) { var offset = opts["offset"]; }
-    
+    if (opts["direction"]) { direction = opts["direction"]; }
+    if (opts["limit"]) { limit = opts["limit"]; }
+    if (opts["offset"]) { offset = opts["offset"]; }
+    console.log(name);
     var url = "https://api.twitch.tv/kraken/users/{name}/follows/channels?direction={direction}&limit={limit}&offset={offset}&sortby=created_at"
                                     .format({direction: direction, name: name, limit: limit, offset: offset })
-    console.log(url);
     
     $.ajax({
         url: "https://api.twitch.tv/kraken/users/{name}/follows/channels?direction={direction}&limit={limit}&offset={offset}&sortby=created_at"
@@ -40,11 +39,10 @@ function getAllFollowedStreams(name, opts, callback) {
         dataType: "jsonp", // tell jQuery we're expecting JSONP. Script and JSONP requests are not subject to the same origin policy restrictions.
 
         success: function(streams) {
-            console.log(streams);
+            callback(streams);
         },
         error: function(streams) {
-            console.log("The request could not be made.")
-            console.log(streams);
+            console.log("The request could not be made.");
         }
     });
 }
@@ -77,13 +75,23 @@ function userExists(name, callback) {
 // User is "finished typing," do something
 // Pass an object with key/values
 // Pass this to verify as a first class citizen to a verify function.
-function doneTyping (dom_id, name) {
+function doneTyping(dom_id, name, opts) {
     console.log(name);
+    var id = null;
+    
     userExists(name, function(result) {
         if (result) { // Success.
             $(dom_id).text("yes");
+            if (opts["users"] && opts["id"]) {
+                opts["users"][opts["id"]] = name;
+            }
+            console.log(finalCheck(users));
         } else {
             $(dom_id).text("no");
+            if (opts["users"] && opts["id"]) {
+                opts["users"][opts["id"]] = null;
+            }
+            console.log(finalCheck(users));
         }
     });
 }
@@ -92,7 +100,7 @@ function doneTyping (dom_id, name) {
  *
  * 
  */
-function verify(input_dom_id, output_dom_id, funcObj, doneTypingInterval ) {
+function verify(input_dom_id, output_dom_id, funcObj, doneTypingInterval) {
     var typingTimer;
     
     // On keyup, start the countdown. Ideally, this happens when the user finishes typing in the user name.
@@ -101,7 +109,9 @@ function verify(input_dom_id, output_dom_id, funcObj, doneTypingInterval ) {
         clearTimeout(typingTimer);
         // Requres function() { funcObj() } for setTimeout to execute interval expires.
         if( $(this).val().length > 0 ) { 
-            typingTimer = setTimeout( function() { funcObj() }, doneTypingInterval);
+            typingTimer = setTimeout( function() 
+             {funcObj() }, doneTypingInterval
+            );
         }
     });
     
@@ -118,15 +128,109 @@ function verify(input_dom_id, output_dom_id, funcObj, doneTypingInterval ) {
     
 }
 
+function finalCheck(users) {
+    for ( x in users ) {
+        if (!users[x]) {
+            return false;   
+        }
+    }
+    return true;
+}
+
+function findMatchingFollows(users, callback) {
+    opts = {"offset":0, "direction":"ASC", "limit":1000}
+    var asyncTasks = [];
+    console.log(users);
+    
+//    for ( key in users ) {
+//        console.log(users[key]);
+//
+//        asyncTasks.push(
+//            function(callback) {
+//                getAllFollowedStreams(users[key], opts, function(result) {
+//                    callback(null, result);
+//                });
+//            }
+//        );
+//    }
+    
+     asyncTasks.push(
+        function(callback) {
+            getAllFollowedStreams(users[1], opts, function(result) {
+                callback(null, result);
+            });
+        }
+    );
+
+     asyncTasks.push(
+        function(callback) {
+            getAllFollowedStreams(users[2], opts, function(result) {
+                callback(null, result);
+            });
+        }
+    );
+
+    async.parallel( asyncTasks,
+        function(err, result) { // Each task will push their callbacks into an array.
+            callback(result);
+        }
+    );
+}
+
+function matchFollowers(input) {
+    // Followers and timestamp.
+//    for (i=0; i < input.follows.length; i++) {
+//        console.log(input.follows[i].channel.name + " " + input.follows[i].created_at);
+//    }
+    var name1;
+    var name2;
+    for (i = 0; i < input[0].follows.length; i++ ) {
+        name1 = input[0].follows[i].channel.name
+        for (x = 0; x < input[1].follows.length; x++) {
+            name2 = input[1].follows[x].channel.name;
+            if ( name1 === name2 ) {
+                console.log(name1);   
+            }
+        }
+    }
+}
+
+var users = {
+    1 : "jackafur",
+    2 : "puri_puri"
+}
+
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "Document is ready!" );
     opts = {"offset":0, "direction":"ASC", "limit":1000}
-    userExists("asdfasdasdfsafas", function(result) {
-        console.log(result)
+
+    // Test.
+//    userExists("asdfasdasdfsafas", function(result) {
+//        console.log(result)
+//    });
+    
+    
+    findMatchingFollows(users, function(result) {
+        console.log(result); // [Object, Object]
+        matchFollowers(result);
     });
-    getAllFollowedStreams("tak_ada", opts);
+    
+    getAllFollowedStreams("serokichimpo", opts, function(result) {
+        console.log(result);
+        matchFollowers(result);
+    });
+    
     // function(){func(args)} syntax to prevent executing function after passing it in as an argument
-    verify("#input1", "#output1", function(){doneTyping("#output1", $("#input1").val())}, 400)
-    verify("#input2", "#output2", function(){doneTyping("#output2", $("#input2").val())}, 400)
+    verify("#input1", "#output1", function(){doneTyping("#output1", $("#input1").val(), {"users":users, "id":1})}, 400);
+    verify("#input2", "#output2", function(){doneTyping("#output2", $("#input2").val(), {"users":users, "id":2})}, 400);
+
+    // Event Handlers, Button clicks, etc.
+    $("#submit").click(function() {
+        $("#result").text(finalCheck(users));
+        findMatchingFollows(users, function(result) {
+            console.log(result); // [Object, Object]
+        });
+    });
+    
 });
